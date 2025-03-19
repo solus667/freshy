b85abf5e3dbfbd02de1f4d605753096c21178bed
solus667 <rmorenox@gmail.com>
2025-03-18 20:44:02 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# fresh install script
+uname -nr
+echo ""
+echo ""
+sleep 1
+echo "|---- Welcome ${USER} ----|"
+echo ""
+echo ""
+sleep 1
+sudo apt update
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+sleep 1
+echo "|---- Download some software ----|"
+sh ./sc/msfonts.sh
+sh ./sc/new.sh
+sh ./sc/chrome.sh
+sh ./sc/vscode.sh
+sh ./sc/ff.sh
+sleep 1
+echo "|---- Remove DEBs ----|"
+echo ""
+echo ""
+sleep 1
+sh ./sc/cleanup.sh
+echo ""
+echo ""
+fastfetch
+echo ""
