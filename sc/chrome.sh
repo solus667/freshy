d5fa89f2826e393ace3dc799e0d6b02c655dc904
solus667 <rmorenox@gmail.com>
2025-03-19 18:34:22 -0700

    Add files via upload
    get some

--- sc/chrome.sh
+++ sc/chrome.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+echo "Chrome?"
+echo -n "0-yes 1-no: "
+read VAR
 
-if [ $USER == s667 ];
+if [ $VAR -eq 0 ] 
 then
-  echo "++ Install Chome Browser ++"
-  echo ""
-  sleep 1
-  wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  sudo apt install ./chrome.deb -y
-  sleep 1
-  echo ""
-  echo "SUCCESS!"
-  echo ""
+  # Install the chrome
+    echo "++ Install Chome Browser ++"
+    echo ""
+    sleep 1
+    wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install ./chrome.deb -y
+    sleep 1
+    echo ""
+    echo "SUCCESS!"
+    echo ""
+elif [ $VAR -eq 1 ]
+then
+  #don't
+    echo ""
+    echo "NO CHROME FOR YOU"
+    echo ""
 else
-  echo "): NO CHROME FOR YOU :("
+  # don't more
+    echo "0 or 1 only dummy"
 fi
