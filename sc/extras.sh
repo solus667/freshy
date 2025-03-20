39535d16c0e6902bc18d8d4b0d382c7037ab97b7
solus667 <rmorenox@gmail.com>
2025-03-19 19:31:51 -0700

    Add files via upload

---
+++ sc/extras.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Want some candy?"
+echo -n "Y-0 or N-1: "
+read VAR
+
+if [ $VAR -eq 0 ]
+then
+  # Some shit
+  sudo apt install cmus cmatrix cbonsai yaru* video-downloader
+elif [ $VAR -eq 0 ]
+then 
+  #No
+  echo "++ Fine then ++"
+  sleep 2
+  echo ":("
+else
+  echo "just answer the question"
+fi
+
+echo ""
+echo ""
+echo "Fonts?"
+echo -n "Y-0 or N-1"
+read VAR
+
+if [ $VAR -eq 0 ]
+then
+  # FONTS
+  sudo apt install fonts-firacode fonts-cascadia-code fonts-jetbrains-mono fonts-mononoki fonts-roboto fonts-hack*
+elif [ $VAR -eq 1 ]
+then
+  echo "++ Fine be boring. ++"
+else
+  echo "dumbass"
+fi
