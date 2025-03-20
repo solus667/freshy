bcb8f296a295c520ed2508e12e0b9190610c7e89
solus667 <rmorenox@gmail.com>
2025-03-19 18:03:15 -0700

    Update chrome.sh
    F225 doesnt want chrome.

--- sc/chrome.sh
+++ sc/chrome.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "Install Chome Browser\n\n"
-sleep 1
-wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sleep 1
-sudo apt install ./chrome.deb -y
-sleep 1
-echo ""
-echo "SUCCESS!"
-echo ""
+
+if [ $USER == s667 ];
+then
+  echo "++ Install Chome Browser ++"
+  echo ""
+  sleep 1
+  wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo apt install ./chrome.deb -y
+  sleep 1
+  echo ""
+  echo "SUCCESS!"
+  echo ""
+else
+  echo "): NO CHROME FOR YOU :("
+fi
