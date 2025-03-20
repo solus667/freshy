8de0d5ab6ce30121b8e6fbe2ca18e46bb52c4cc5
solus667 <rmorenox@gmail.com>
2025-03-19 17:02:14 -0700

    Update msfonts.sh
    ad deb

--- sc/msfonts.sh
+++ sc/msfonts.sh
@@ -5,7 +5,14 @@ release_file=/etc/os-release
 if grep -q "Linux Mint" $release_file
 then
 	# Microsoft fonts
+ 	# Spotify and steam for mint only
 	sudo apt install ttf-mscorefonts-installer spotify-client steam -y
+fi
+
+if grep -q "Debian" $release_file
+then
+	# Microsoft fonts
+ 	sudo apt install ttf-mscorefonts-installer -y
 fi
  echo ""
  echo "SUCCESS!"
