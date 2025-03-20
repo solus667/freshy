e6fa2300dad9747d7a1a30f9caa5ba6411ff415e
solus667 <rmorenox@gmail.com>
2025-03-20 14:41:45 -0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,12 +4,11 @@
 uname -nr
 echo ""
 sleep 1
-echo "++ Welcome ${USER} ++"
+echo "++ Welcome ${USER} ++"|pv -qL 10
 echo ""
 echo ""
 sleep 1
 sudo apt update
-sudo apt install vim vim-scripts
 sudo apt upgrade -y
 sudo apt full-upgrade -y
 sleep 1
