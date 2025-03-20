41e7458cdb7f598861f80bd1a4c5ed3c82fb0d70
solus667 <rmorenox@gmail.com>
2025-03-19 19:36:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,8 @@
 # fresh install script
 uname -nr
 echo ""
-echo ""
 sleep 1
-echo "|---- Welcome ${USER} ----|"
+echo "++ Welcome ${USER} ++"
 echo ""
 echo ""
 sleep 1
@@ -13,14 +12,15 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt full-upgrade -y
 sleep 1
-echo "|---- Download some software ----|"
-sh ./sc/chrome.sh
-sh ./sc/msfonts.sh
+echo ""
+echo "++ Download some software ++"
 sh ./sc/new.sh
+sh ./sc/chrome.sh
 sh ./sc/vscode.sh
 sh ./sc/ff.sh
 sleep 1
-echo "|---- Remove DEBs ----|"
+echo ""
+echo "++ Remove DEBs ++"
 echo ""
 echo ""
 sleep 1
