bfe6b700a75015828d8258b71bb6448f9a5df659
solus667 <rmorenox@gmail.com>
2025-03-19 18:41:58 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ sudo apt upgrade -y
 sudo apt full-upgrade -y
 sleep 1
 echo "|---- Download some software ----|"
+sh ./sc/chrome.sh
 sh ./sc/msfonts.sh
 sh ./sc/new.sh
-sh ./sc/chrome.sh
 sh ./sc/vscode.sh
 sh ./sc/ff.sh
 sleep 1
